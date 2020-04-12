5d17e5397d1c6b7a7cb31559361f0b2fd5aa8070
bausano <michael@enhance.com>
2020-04-12 15:57:07 +0100

    Using typeform interface to validate the request.

--- lib/anypro_web/controllers/coach_controller.ex
+++ lib/anypro_web/controllers/coach_controller.ex
@@ -1,43 +1,11 @@
 defmodule AnyproWeb.CoachController do
   use AnyproWeb, :controller
-
+  alias Anypro.Coach
+  alias Anypro.Repo
   # Imports the "last" function.
   import Ecto.Query
 
-  alias Anypro.Coach
-  alias Anypro.Repo
-
-  # The expected request JSON which is sent to the store webhook.
-  @typeform_store_schema %{
-    "type" => "object",
-    "required" => ["form_response"],
-    "properties" => %{
-      "form_response" => %{
-        "type" => "object",
-        "required" => ["answers"],
-        "properties" => %{
-          "answers" =>  %{"items" => %{
-            "type" => "object",
-            "required" => ["field"],
-            "properties" => %{
-              "email" => %{"type" => "string"},
-              "phone_number" => %{"type" => "string"},
-              "text" => %{"type" => "string"},
-              "file_url" => %{"type" => "string"},
-              "boolean" => %{"type" => "boolean"},
-              "field" => %{
-                "type" => "object",
-                "required" => ["ref"],
-                "properties" => %{
-                  "ref" => %{"type" => "string"}
-                }
-              }
-            }
-          }}
-        }
-      }
-    }
-  } |> ExJsonSchema.Schema.resolve()
+  plug AnyproWeb.Plugs.Typeform when action in [:store]
 
   # Views a coach profile.
   def show(conn, %{"slug" => slug}) do
@@ -55,57 +23,50 @@ defmodule AnyproWeb.CoachController do
     end
   end
 
-  # Inserts a new coach into the database.
-  def store(conn, params) do
-    # TODO: Do validation as a middleware.
-    case ExJsonSchema.Validator.validate(@typeform_store_schema, params) do
-      {:error, errors} ->
-        IO.inspect errors
-        IO.inspect params
-        send_resp(conn, :unprocessable_entity, "")
-      :ok ->
-        answers = params["form_response"]["answers"]
+  # Inserts a new coach into the database. This method goes through Typeform
+  # plug which validates the request and assignes the answers to the conn.
+  def store(conn, _params) do
+    answers = conn.assigns.answers
 
-        # Finds the answer which matches given ref. We set up refs in the
-        # typeform UI. The map_res is either a map or an atom :error.
-        answer_for_field = fn map_res, ref, type ->
-          case map_res do
-            :error -> :error
-            map ->
-              answer = Enum.find(answers, fn a -> a["field"]["ref"] == ref end)
-              case answer do
-                nil -> :error
-                # Gets the answer value based on the provided type. For example, for
-                # a phone number, the type would be "phone_number" whereas for name
-                # it would be "text".
-                a -> Map.put(map, ref, a[type])
-              end
+    # Finds the answer which matches given ref. We set up refs in the
+    # typeform UI. The map_res is either a map or an atom :error.
+    answer_for_field = fn map_res, ref, type ->
+      case map_res do
+        :error -> :error
+        map ->
+          answer = Enum.find(answers, fn a -> a["field"]["ref"] == ref end)
+          case answer do
+            nil -> :error
+            # Gets the answer value based on the provided type. For example, for
+            # a phone number, the type would be "phone_number" whereas for name
+            # it would be "text".
+            a -> Map.put(map, ref, a[type])
           end
-        end
+      end
+    end
 
-        # Gets all required fields from the form. If any field is missing, the
-        # coach will be equal to atom :error.
-        coach = Map.new()
-        |> answer_for_field.("bio", "text")
-        |> answer_for_field.("email", "email")
-        |> answer_for_field.("name", "text")
-        |> answer_for_field.("pga_qualified", "boolean")
-        |> answer_for_field.("phone", "phone_number")
-        |> answer_for_field.("pricing", "text")
-        |> answer_for_field.("profile_picture", "file_url")
+    # Gets all required fields from the form. If any field is missing, the
+    # coach will be equal to atom :error.
+    coach = Map.new()
+    |> answer_for_field.("bio", "text")
+    |> answer_for_field.("email", "email")
+    |> answer_for_field.("name", "text")
+    |> answer_for_field.("pga_qualified", "boolean")
+    |> answer_for_field.("phone", "phone_number")
+    |> answer_for_field.("pricing", "text")
+    |> answer_for_field.("profile_picture", "file_url")
 
-        case coach do
-          :error -> send_resp(conn, :unprocessable_entity, "")
-          coach ->
-            insert_coach_with_unique_slug(
-              conn,
-              Coach.slugified_name(coach["name"]),
-              # Don't put any prefix to the slug initially and hope it's unique.
-              "",
-              # Maps all string keys to atoms.
-              Map.new(coach, fn {k, v} -> { String.to_atom(k), v } end)
-            )
-        end
+    case coach do
+      :error -> send_resp(conn, :unprocessable_entity, "")
+      coach ->
+        insert_coach_with_unique_slug(
+          conn,
+          Coach.slugified_name(coach["name"]),
+          # Don't put any prefix to the slug initially and hope it's unique.
+          "",
+          # Maps all string keys to atoms.
+          Map.new(coach, fn {k, v} -> { String.to_atom(k), v } end)
+        )
     end
   end
 
---
+++ lib/anypro_web/plugs/typeform.ex
@@ -0,0 +1,53 @@
+defmodule AnyproWeb.Plugs.Typeform do
+  import Plug.Conn
+
+  # The expected request JSON which is sent to typeform webhooks.
+  @typeform_schema %{
+    "type" => "object",
+    "required" => ["form_response"],
+    "properties" => %{
+      "form_response" => %{
+        "type" => "object",
+        "required" => ["answers"],
+        "properties" => %{
+          "answers" =>  %{"items" => %{
+            "type" => "object",
+            "required" => ["field"],
+            "properties" => %{
+              "email" => %{"type" => "string"},
+              "phone_number" => %{"type" => "string"},
+              "text" => %{"type" => "string"},
+              "file_url" => %{"type" => "string"},
+              "boolean" => %{"type" => "boolean"},
+              "field" => %{
+                "type" => "object",
+                "required" => ["ref"],
+                "properties" => %{
+                  "ref" => %{"type" => "string"}
+                }
+              }
+            }
+          }}
+        }
+      }
+    }
+  } |> ExJsonSchema.Schema.resolve()
+
+
+  # TODO: Use the example init function at https://hexdocs.pm/phoenix/plug.html
+  # to implement token checking.
+  def init([]), do: false
+
+  def call(%Plug.Conn{params: params} = conn, _init) do
+    case ExJsonSchema.Validator.validate(@typeform_schema, params) do
+      {:error, errors} ->
+        IO.inspect errors
+        IO.inspect params
+        conn
+        |> send_resp(:unprocessable_entity, "")
+        |> halt()
+      :ok ->
+        assign(conn, :answers, params["form_response"]["answers"])
+    end
+  end
+end
